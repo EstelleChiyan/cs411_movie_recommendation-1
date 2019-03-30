2fae7fb44aba3df5d6274749760523ff83a6483c
baohe-zhang <baohez2@illinois.edu>
2019-03-30 03:25:00 -0500

    add \n

--- movie-recommendation/src/main/java/com/idk/movierecommendation/MovieDetailDAO.java
+++ movie-recommendation/src/main/java/com/idk/movierecommendation/MovieDetailDAO.java
@@ -30,7 +30,7 @@ public class MovieDetailDAO {
         int update = jdbcTemplate.update(sql, movie.getId(), movie.getTitle(), movie.getOverview(), movie.getPosterPath(),
                 movie.getReleaseDate(), movie.getRuntime(), movie.getOriginalLanguage());
         if (update == 1) {
-            System.out.printf("Movie %s is inserted", movie.getTitle());
+            System.out.printf("Movie %s is inserted\n", movie.getTitle());
         }
         return movie;
     }
@@ -44,17 +44,16 @@ public class MovieDetailDAO {
         String deleteSQL = "DELETE FROM movies WHERE id=?";
         int update = jdbcTemplate.update(deleteSQL, id);
         if (update == 1) {
-            System.out.printf("Movie %s is deleted", movie.getTitle());
+            System.out.printf("Movie %s is deleted\n", movie.getTitle());
         }
         return movie;
     }
 
     public MovieDetailModel updateMovie(MovieDetailModel movie){
-        //String UPDATE_EMPLOYEE_SQL = "UPDATE employee_table set email=? WHERE employee_id=?";
         String updateSQL = "UPDATE movies SET title=?, overview=?, poster_path=?, release_date=?, runtime=?, original_language=? WHERE id=?";
         int update = jdbcTemplate.update(updateSQL, movie.getTitle(), movie.getOverview(), movie.getPosterPath(), movie.getReleaseDate(), movie.getRuntime(), movie.getOriginalLanguage(), movie.getId());
         if(update == 1){
-            System.out.printf("Movie %s is updated", movie.getTitle());
+            System.out.printf("Movie %s is updated\n", movie.getTitle());
         }
         return movie;
     }
