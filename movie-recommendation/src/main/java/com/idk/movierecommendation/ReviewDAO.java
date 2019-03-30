72ac21cfac1a7bfa77f261de1c131f8fdd725b23
EstelleChiyan <yuyingc3@illinois.edu>
2019-03-30 03:43:22 -0500

    ang?

--- movie-recommendation/src/main/java/com/idk/movierecommendation/ReviewDAO.java
+++ movie-recommendation/src/main/java/com/idk/movierecommendation/ReviewDAO.java
@@ -6,6 +6,23 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class ReviewDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    public List<ReviewModel> getReviewByMovie(int movieId){
+        String sql = "SELECT * FROM reviews WHERE movies_id=?";
+        List<ReviewModel> reviewList = jdbcTemplate.query(sql, new ReviewRowMapper(), movieId);
+        return reviewList;
+    }
+
+    public ReviewModel postReview(ReviewModel reviewModel){
+        String sql = "INSERT INTO reviews VALUES(?,?,?,?,?)";
+        int update = jdbcTemplate.update(sql, reviewModel.getId(), reviewModel.getContent(), reviewModel.getPostDate(), reviewModel.getMoviesId(), reviewModel.getUsersId());
+        if (update == 1) {
+            System.out.printf("Review %s is posted", reviewModel.getContent());
+        }
+        return reviewModel;
+    }
 }
