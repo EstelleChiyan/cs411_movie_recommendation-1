29cd025a5caf2fd503ae9a62358f3804a0b0b6a5
wuwuwujy <wujiaying211@gmail.com>
2019-04-16 20:41:18 -0500

    backend
    add review&user

--- movie-recommendation/src/main/java/com/idk/movierecommendation/ReviewDAO.java
+++ movie-recommendation/src/main/java/com/idk/movierecommendation/ReviewDAO.java
@@ -11,10 +11,28 @@ public class ReviewDAO {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    public ReviewModel getReviewById(String id) {
+        String sql = "SELECT * FROM reviews WHERE id=?";
+        ReviewModel review = jdbcTemplate.queryForObject(sql, new ReviewRowMapper(), id);
+        return review;
+    }
+
     public List<ReviewModel> getReviewByMovie(int movieId){
         String sql = "SELECT * FROM reviews WHERE movies_id=?";
         List<ReviewModel> reviewList = jdbcTemplate.query(sql, new ReviewRowMapper(), movieId);
         return reviewList;
+    }
+
+    public List<ReviewModel> getReviewByUser(int userId){
+        String sql = "SELECT * FROM reviews WHERE users_id=?";
+        List<ReviewModel> reviewList = jdbcTemplate.query(sql, new ReviewRowMapper(), userId);
+        return reviewList;
+    }
+
+    public int getUserByName(String userName){
+        String sql= "SELECT * FROM users WHERE username=?";
+        UserModel userModel = jdbcTemplate.queryForObject(sql, new UserRowMapper(), userName);
+        return userModel.getId();
     }
 
     public ReviewModel postReview(ReviewModel reviewModel){
@@ -24,5 +42,35 @@ public class ReviewDAO {
             System.out.printf("Review %s is posted", reviewModel.getContent());
         }
         return reviewModel;
+    }
+
+    public ReviewModel deleteReviewById(String id) {
+        String selectSQL = "SELECT * FROM reviews WHERE id=?";
+        ReviewModel review = jdbcTemplate.queryForObject(selectSQL, new ReviewRowMapper(), id);
+        if (review == null) {
+            return null;
+        }
+        String deleteSQL = "DELETE FROM reviews WHERE id=?";
+        int update = jdbcTemplate.update(deleteSQL, id);
+//        if (update == 1) {
+//            System.out.printf("Movie %s is deleted\n", review.getId());
+//        }
+        return review;
+    }
+
+
+    private String id;
+    private String content;
+    private Date postDate;
+    private int moviesId;
+    private int usersId;
+
+    public ReviewModel updateReview(ReviewModel review){
+        String updateSQL = "UPDATE reviews SET content=?, post_date=?, movies_id=?, users_id=? WHERE id=?";
+        int update = jdbcTemplate.update(updateSQL, review.getContent(), review.getPostDate(), review.getMoviesId(), review.getUsersId(), review.getId());
+//        if(update == 1){
+//            System.out.printf("Movie %s is updated\n", movie.getTitle());
+//        }
+        return review;
     }
 }
---
+++ movie-recommendation/src/main/java/com/idk/movierecommendation/UserModel.java
@@ -0,0 +1,57 @@
+package com.idk.movierecommendation;
+
+public class UserModel {
+
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    protected UserModel(){
+
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "ReviewModel{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\''+
+                ", password=" + password + '\''+
+                ", email=" + email +'\''+
+                '}';
+    }
+
+
+}
